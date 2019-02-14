09c15e7c8f8cac16eb65509c7e5cee8340a9ba1a
MadOrni <36563266+MadOrni@users.noreply.github.com>
2019-02-14 10:14:56 +0100

    Update entrypoint.sh
    added  #!/bin/bash

--- latest/entrypoint.sh
+++ latest/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 curl -i -H "Accept: application/json" -H "Content-Type: application/json" https://bdockerapi20181229065149.azurewebsites.net/api/values
 
 echo "this image actually does nothing, its part of a project which tests  user awareness"
